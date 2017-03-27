60f3cae2d4b416976d99ab631dcd8d3543eeb75e
Khaled Mohammad <itskhaledmohammad@gmail.com>
2017-03-27 18:00:48 +0600

    Subtle changes.

--- bluetooth_rc.ino
+++ bluetooth_rc.ino
@@ -1,5 +1,9 @@
-int state = 0;
-int motorPins[] = {8, 9, 10 ,11};
+// Global Variables.
+int state = 0, motorPins[] = {8, 9, 10 ,11};
+
+/*
+ *  This function moves the car forward.
+ */
 void forward()
 {
   digitalWrite(motorPins[0], HIGH);
@@ -7,6 +11,10 @@ void forward()
   digitalWrite(motorPins[2], HIGH);
   digitalWrite(motorPins[3], LOW);
 }
+
+/*
+ * The function move the car backward.
+ */
 void backward()
 {
   digitalWrite(motorPins[0], LOW);
@@ -14,6 +22,10 @@ void backward()
   digitalWrite(motorPins[2], LOW);
   digitalWrite(motorPins[3], HIGH);
 }
+
+/*
+ * This function moves the car left.
+ */
 void left()
 {
   digitalWrite(motorPins[0], LOW);
@@ -21,6 +33,10 @@ void left()
   digitalWrite(motorPins[2], HIGH);
   digitalWrite(motorPins[3], LOW);
 }
+
+/*
+ * This function moves the car right.
+ */
 void right()
 {
   digitalWrite(motorPins[0], HIGH);
@@ -28,41 +44,43 @@ void right()
   digitalWrite(motorPins[2], LOW);
   digitalWrite(motorPins[3], LOW);
 }
-void headLight(int switchState)
-{   
-}
 
 void setup()
 {
+  // Setting up the serial monitor.
   Serial.begin(9600);
+
+  // Setting the motor pins to output.
   for(int i = 8; i <= 11; i++){
     pinMode(i, OUTPUT);
   }
 }
+
 void loop()
 {
-  
+  // Check if we have any data to process.  
   if(Serial.available() > 0)
   {
+
+    // Reading the data.
     state = Serial.read();
-    if(state == 'F')
-    {
+
+    // Take action acording to the ASCII value recieved. 
+    if(state == 'F'){
       forward();
     }
-    else if(state == 'B')
-    {
+    else if(state == 'B'){
       backward();
     }
-    else if(state == 'L')
-    {
+    else if(state == 'L'){
       left();
     }
-    else if(state == 'R')
-    {
+    else if(state == 'R'){
       right();
     }
-    if(state == 'S')
-    {
+
+    // Stop all the motors if no input is given by the user.
+    if(state == 'S'){
       Serial.println(state);  
       for(int i = 0; i < 4; i++)
       {
@@ -70,11 +88,4 @@ void loop()
       }   
     }
   }
-
-  /*
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
-  digitalWrite(11, LOW); */
-
 }
